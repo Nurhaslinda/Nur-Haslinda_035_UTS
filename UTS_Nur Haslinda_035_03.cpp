9afaf17b480f90d273c2e82c8275fcf961c727b2
Nurhaslinda <89765164+Nurhaslinda@users.noreply.github.com>
2021-10-11 09:39:54 +0700

    Add files via upload

---
+++ UTS_Nur Haslinda_035_03.cpp
@@ -0,0 +1,35 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    char operasi;
+    float a, b;
+	cout << "Enter any two number: ";
+    cin >> a >> b;
+    cout << "Enter the operator (+, -, *, /): ";
+    cin >> operasi;
+    switch(operasi) {
+        case '+':
+            cout << "The calculated result is : " << a + b;
+            break;
+
+        case '-':
+            cout << "The calculated result is : " << a - b;
+            break;
+
+        case '*':
+            cout << "The calculated result is : " << a * b;
+            break;
+
+        case '/':
+            cout << "The calculated result is : " << a / b;
+            break;
+
+        default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct";
+            break;
+    }
+    
+    return 0;
+}
